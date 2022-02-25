99f623f022fcc6d825cf2e3c2f00138fb0485254
Atsushi Morimoto (74th) <site@74th.tech>
2022-02-25 12:29:09 +0900

    publish

--- code.py
+++ code.py
@@ -1,18 +1,22 @@
 from adafruit_mcp230xx.mcp23017 import MCP23017
 import board
 import busio
+import digitalio
 
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.matrix import DiodeOrientation
 from kmk.modules.layers import Layers
-
-from keymap import get_keymap
+from kmk.modules.modtap import ModTap
 
 keyboard = KMKKeyboard()
 keyboard.modules.append(Layers())
+modtap = ModTap()
+keyboard.modules.append(modtap)
 
-i2c = busio.I2C(board.GP13, board.GP12)
+from keymap import get_keymap
+
+i2c = busio.I2C(board.GP13, board.GP12, frequency=300_000)
 mcp = MCP23017(i2c)
 
 
@@ -25,6 +29,7 @@ def get_wrapper_class():
             self.base = base
 
         def switch_to_input(self, pull):
+            # skip pull-down register
             self.base.switch_to_input()
 
         def switch_to_output(self):
@@ -40,24 +45,52 @@ def get_wrapper_class():
 
     return DigitalInOut
 
+def get_dual_output_class():
+    class DigitalInOut:
+        def __init__(
+            self,
+            base1,
+            base2,
+        ):
+            if base1.__class__.__name__ == 'DigitalInOut':
+                self.base1 = base1
+            else:
+                self.base1 = digitalio.DigitalInOut(base1)
+            if base2.__class__.__name__ == 'DigitalInOut':
+                self.base2 = base2
+            else:
+                self.base2 = digitalio.DigitalInOut(base2)
 
-MCP23017DigitalInOutWrapper = get_wrapper_class()
+        def switch_to_input(self, pull):
+            raise Exception("no input")
+
+        def switch_to_output(self):
+            self.base1.switch_to_output()
+            self.base2.switch_to_output()
+
+        def get_value(self):
+            raise Exception("no input")
+
+        def set_value(self, value):
+            self.base1.value = value
+            self.base2.value = value
+
+        value = property(get_value, set_value)
+
+    return DigitalInOut
+
+
+WrappedDigitalInOut = get_wrapper_class()
+DualDigitalOut = get_dual_output_class()
 
 keyboard.col_pins = [
-    board.GP5,
-    board.GP6,
-    board.GP7,
-    board.GP8,
-    board.GP9,
-    board.GP10,
-    board.GP11,
-    MCP23017DigitalInOutWrapper(mcp.get_pin(8)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(9)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(10)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(11)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(12)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(13)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(14)),
+    DualDigitalOut(board.GP5, mcp.get_pin(8)),
+    DualDigitalOut(board.GP6, mcp.get_pin(9)),
+    DualDigitalOut(board.GP7, mcp.get_pin(10)),
+    DualDigitalOut(board.GP8, mcp.get_pin(11)),
+    DualDigitalOut(board.GP9, mcp.get_pin(12)),
+    DualDigitalOut(board.GP10, mcp.get_pin(13)),
+    DualDigitalOut(board.GP11, mcp.get_pin(14)),
 ]
 mcp.iodirb = 0
 
@@ -67,11 +100,11 @@ keyboard.row_pins = [
     board.GP2,
     board.GP3,
     board.GP4,
-    MCP23017DigitalInOutWrapper(mcp.get_pin(0)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(1)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(2)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(3)),
-    MCP23017DigitalInOutWrapper(mcp.get_pin(4)),
+    WrappedDigitalInOut(mcp.get_pin(0)),
+    WrappedDigitalInOut(mcp.get_pin(1)),
+    WrappedDigitalInOut(mcp.get_pin(2)),
+    WrappedDigitalInOut(mcp.get_pin(3)),
+    WrappedDigitalInOut(mcp.get_pin(4)),
 ]
 mcp.iodira = 1
 n_rows = len(keyboard.row_pins)
@@ -87,11 +120,11 @@ left_map = [
     [                                               (3,4),      (4,4),      (5,4),      (6,4)],
 ]
 right_map = [
-    [                       (8,5),      (9,5),      (10,5),     (11,5),     (12,5),     (13,5)],
-    [(7,5),     (7,6),      (8,6),      (9,6),      (10,6),     (11,6),     (12,6),     (13,6)],
-    [           (7,7),      (8,7),      (9,7),      (10,7),     (11,7),     (12,7),     (13,7)],
-    [           (7,8),      (8,8),      (9,8),      (10,8),     (11,8),     (12,8),     (13,8)],
-    [           (7,9),      (8,9),      (9,9),      (10,9)],
+    [                       (1,5),      (2,5),      (3,5),      (4,5),      (5,5),      (6,5)],
+    [(0,5),     (0,6),      (1,6),      (2,6),      (3,6),      (4,6),      (5,6),      (6,6)],
+    [           (0,7),      (1,7),      (2,7),      (3,7),      (4,7),      (5,7),      (6,7)],
+    [           (0,8),      (1,8),      (2,8),      (3,8),      (4,8),      (5,8),      (6,8)],
+    [           (0,9),      (1,9),      (2,9),      (3,9)],
 ]
 
 def apply_to_map(
--- keymap.py
+++ keymap.py
@@ -1,24 +1,151 @@
 from kmk.keys import KC
+from kmk.handlers.sequences import simple_key_sequence, send_string
+from secret import input_otp1, input_otp2
 
-base_mac = 0
-linux = 0
 
-base_mac_left = [
-    [KC.ESC,    KC.GRV,     KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5],
-    [           KC.TAB,     KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,       KC.N6],
-    [           KC.LCTL,    KC.A,       KC.S,       KC.D,       KC.F,       KC.G,       KC.LBRC],
-    [           KC.LSFT,    KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,       KC.RBRC],
-    [                                               KC.ESC,     KC.LGUI,    KC.LCTL,    KC.SPC],
-]
-base_mac_right = [
-    [                       KC.N7,      KC.N8,      KC.N9,      KC.N0,      KC.MINS,    KC.EQL],
-    [KC.F2,     KC.F3,      KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.BSLS],
-    [           KC.BSPC,    KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.QUOT],
-    [           KC.ENT,     KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    KC.F12],
-    [           KC.RALT,    KC.RGUI,    KC.RCTL,    KC.RCTL],
-]
+def S(kc):
+    return KC.LSFT(kc)
+
+def C(kc):
+    return KC.LCTL(kc)
+
+def A(kc):
+    return KC.LALT(kc)
+
+def G(kc):
+    return KC.LGUI(kc)
+
+def CA(kc):
+    return KC.LCTL(KC.LALT(kc))
+
+def SG(kc):
+    return KC.LSFT(KC.LGUI(kc))
+
+
+mac_base_layer = 0
+mac_raise_layer = 1
+linux_base_layer = 2
+linux_raise_layer = 3
+special_layer = 4
+
+
+____ = KC.TRANSPARENT
+
+special = KC.MO(special_layer)
+
+def mac_keymap():
+    eisu = KC.LANG2
+    kana = KC.LANG1
+    pr_window = SG(KC.N3)
+    pr_screen = SG(KC.N4)
+
+    ctl_tab = KC.MT(KC.TAB, KC.LCTRL, prefer_hold=True)
+    cmd_eisu = KC.MT(eisu, KC.LGUI, prefer_hold=True)
+    opt_kana = KC.MT(kana, KC.LALT, prefer_hold=True)
+    esc_eisu = simple_key_sequence([eisu, KC.ESC])
+    raise_ent = KC.LT(mac_raise_layer, KC.ENT, prefer_hold=True)
+
+    base_left = [
+        [special,   KC.GRAVE,   KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5],
+        [           KC.GRAVE,   KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,       KC.N6],
+        [           ctl_tab,    KC.A,       KC.S,       KC.D,       KC.F,       KC.G,       KC.LBRC],
+        [           KC.LSHIFT,  KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,       KC.RBRC],
+        [                       esc_eisu,   ctl_tab,    cmd_eisu,   KC.SPC],
+    ]
+    base_right = [
+        [                       KC.N7,      KC.N8,      KC.N9,      KC.N0,      KC.MINS,    KC.EQL],
+        [KC.F2,     KC.F3,      KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.EQL],
+        [           KC.BSPC,    KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.QUOT],
+        [           KC.ENT,     KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    KC.BSLS],
+        [           KC.ENTER,   raise_ent,  opt_kana,   KC.BSPC],
+    ]
+
+    raise_left = [
+        [____,      CA(KC.Q),   G(KC.F1),   SG(KC.F2),  G(KC.F3),   G(KC.F4),   CA(KC.ESC)],
+        [           KC.ESC,     KC.F1,      KC.F2,      KC.F3,      KC.F4,      KC.F5,      KC.F6],
+        [           KC.LCTL,    S(KC.N1),   S(KC.N2),   S(KC.N3),   S(KC.N4),   S(KC.N5),   S(KC.N6)],
+        [           KC.LSHIFT,  ____,       ____,       pr_window,  pr_screen,  KC.F12,     ____],
+        [                       esc_eisu,   KC.SPC,     KC.LGUI,    KC.MINS],
+    ]
+    raise_right = [
+        [                       G(KC.F6),   G(KC.F7),   G(KC.F8),   G(KC.F9),   ____,       ____],
+        [KC.F2,     KC.F3,      KC.F7,      KC.F8,      KC.F9,      KC.F10,     KC.F11,     KC.F12],
+        [           ____,       S(KC.N7),   S(KC.N8),   S(KC.N9),   S(KC.N0),   S(KC.MINS), KC.EQL],
+        [           ____,       KC.LEFT,    KC.DOWN,    KC.UP,      KC.RIGHT,   KC.HOME,    KC.END],
+        [           ____,       ____,       ____,       KC.DEL],
+    ]
+    return [[base_left, base_right], [raise_left, raise_right]]
+
+def linux_keymap():
+    eisu = KC.MHEN
+    kana = KC.HENK
+    pr_window = A(KC.PSCREEN)
+    pr_screen = S(KC.PSCREEN)
+
+    alt_tab = KC.MT(KC.TAB, KC.LALT, prefer_hold=True)
+    ctl_tab = KC.MT(KC.TAB, KC.LCTRL, prefer_hold=True)
+    ctl_eisu = KC.MT(eisu, KC.LCTRL, prefer_hold=True)
+    gui_kana = KC.MT(kana, KC.LGUI, prefer_hold=True)
+    esc_eisu = simple_key_sequence([eisu, KC.ESC])
+    raise_ent = KC.LT(linux_raise_layer, KC.ENT, prefer_hold=True)
+
+    base_left = [
+        [special,   KC.GRAVE,   KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5],
+        [           KC.GRAVE,   KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,       KC.N6],
+        [           ctl_tab,    KC.A,       KC.S,       KC.D,       KC.F,       KC.G,       KC.LBRC],
+        [           KC.LSHIFT,  KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,       KC.RBRC],
+        [                       esc_eisu,   alt_tab,    ctl_eisu,   KC.SPC],
+    ]
+    base_right = [
+        [                       KC.N7,      KC.N8,      KC.N9,      KC.N0,      KC.MINS,    KC.EQL],
+        [KC.F2,     KC.F3,      KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.EQL],
+        [           KC.BSPC,    KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.QUOT],
+        [           KC.ENT,     KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    KC.BSLS],
+        [           KC.ENTER,   raise_ent,  gui_kana,   KC.BSPC],
+    ]
+    raise_left = [
+        [____,      CA(KC.F2),  G(KC.F1),   SG(KC.F2),  G(KC.F3),   G(KC.F4),   CA(KC.ESC)],
+        [           KC.ESC,     KC.F1,      KC.F2,      KC.F3,      KC.F4,      KC.F5,      KC.F6],
+        [           KC.GRV,     S(KC.N1),   S(KC.N2),   S(KC.N3),   S(KC.N4),   S(KC.N5),   S(KC.N6)],
+        [           KC.LSHIFT,  ____,       ____,       pr_window,  pr_screen,  KC.F12,     ____],
+        [                       KC.ESC,     KC.LALT,    KC.LCTL,     KC.MINS],
+    ]
+    raise_right = [
+        [                       G(KC.F6),   G(KC.F7),   G(KC.F8),   G(KC.F9),   ____,       ____],
+        [KC.F2,     KC.F3,      KC.F7,      KC.F8,      KC.F9,      KC.F10,     KC.F11,     KC.F12],
+        [           ____,       S(KC.N7),   S(KC.N8),   S(KC.N9),   S(KC.N0),   S(KC.MINS), KC.EQL],
+        [           ____,       KC.LEFT,    KC.DOWN,    KC.UP,      KC.RIGHT,   KC.HOME,    KC.END],
+        [           ____,       ____,       ____,       KC.DEL],
+    ]
+    return [[base_left, base_right], [raise_left,raise_right]]
+
+def special_keymap():
+    to_mac = KC.DF(mac_base_layer)
+    to_linux = KC.DF(linux_base_layer)
+
+    raise_left = [
+        [____,      ____,       to_mac,     to_linux,   ____,       ____,       ____],
+        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
+        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
+        [           ____,       input_otp2, input_otp1, ____,       ____,       ____,       ____],
+        [                       ____,       ____,       ____,       ____],
+    ]
+    raise_right = [
+        [                       ____,       ____,       ____,       ____,       ____,       ____],
+        [____,      ____,       ____,       ____,       ____,       ____,       ____,       ____],
+        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
+        [           ____,       ____,       ____,       ____,       ____,       ____,       ____],
+        [           ____,       ____,       ____,       ____],
+    ]
+    return [raise_left,raise_right]
 
 def get_keymap():
+    mac_base, mac_raise = mac_keymap()
+    linux_base, linux_raise = linux_keymap()
     return [
-        [base_mac_left, base_mac_right]
+        mac_base,
+        mac_raise,
+        linux_base,
+        linux_raise,
+        special_keymap(),
     ]
